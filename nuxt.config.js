73d45075d35fc32eb2e451d9ac47d02d586b5e5b
Thibeault Chenu <contact@tchenu.com>
2020-10-01 13:55:32 +0200

    :lipstick: Updating favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,13 +14,14 @@ export default {
           "fullstack developer at SVP. I'm also a hacker at night and on weekends.",
       },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+    ],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-    '~/assets/main.css',
-  ],
+  css: ['~/assets/main.css'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
