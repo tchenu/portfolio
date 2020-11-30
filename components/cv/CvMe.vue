44ce1b817166607eecb90ba3bc9ed6d420cf7798
Thibeault Chenu <contact@tchenu.com>
2020-11-30 20:04:18 +0100

    :sparkles: Introducing new PP

--- components/cv/CvMe.vue
+++ components/cv/CvMe.vue
@@ -2,7 +2,7 @@
   <div class="p-5 border-b border-grey-100">
     <div class="container flex items-center">
       <div class="h-32 w-32 md:h-32 md:w-32">
-        <img class="object-cover rounded-full" src="~/assets/images/pp.png" />
+        <img class="object-cover rounded-full" src="~/assets/images/pp.jpeg" />
       </div>
       <div class="ml-5">
         <h1 class="text-2xl font-semibold">Thibeault Chenu</h1>
